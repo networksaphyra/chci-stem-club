04132fa9e8e71f59cb3dff7f4afbf74196af337f
networksaphyra <networksaphyra004@gmail.com>
2024-10-17 23:20:54 -0400

    first commit: jsx directory.

---
+++ js/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../css/index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Hello Update!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
