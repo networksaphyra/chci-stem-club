aeed2739febe8ac532afae3468602d02c29661a2
networksaphyra <networksaphyra004@gmail.com>
2024-10-23 23:22:38 -0400

    update: use svg, change logo to remove excessive border, create main content component.

--- js/App.js
+++ js/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header.js";
+import Main from "./Main.js";
 import "../css/index.css";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ js/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/logo2.svg";
+
+const Main = () => {
+  return (
+    <main>
+      <img src={logo} alt="CHCI Stem" className="logo" />
+      <p>Hello World.</p>
+    </main>
+  );
+};
+
+export default Main;
